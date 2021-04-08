457c07b657a09dd950334b7b8eb8e3695d17be63
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-04-08 14:03:29 +0200

    clean up with useEffect, work with refs, delete ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,19 +7,46 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  
+
+  useEffect(()=> {
+    console.log('RENDERING INGREDIENTS', userIngredients)
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients)
+  }, []);
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients, 
-      {id: Math.random().toString(), ...ingredient}])
+    fetch('https://react-hooks-update-9f52a-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients, 
+        {id: responseData.name, ...ingredient}])
+    })
+    
   }
+
+  const removeIngredientHandler = ingredientId => {
+    fetch(`https://react-hooks-update-9f52a-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`, {
+      method: 'DELETE',
+    }).then(response => {
+      setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+    });
+  };
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
-        <IngredientList ingredients = {userIngredients} onRemoveItem={() => {}}/>
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList ingredients = {userIngredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
