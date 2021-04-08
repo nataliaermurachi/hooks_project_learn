572cca3c482c1dab5d8590d64df7b60e2835c249
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-04-08 11:51:55 +0200

    added first list items using useState hook

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,12 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-  useState({ title: "", amount: "" });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount})
   };
 
   return (
@@ -16,11 +18,19 @@ const IngredientForm = React.memo((props) => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input 
+              type="text" 
+              id="title" 
+              value={enteredTitle} 
+              onChange={ event =>setEnteredTitle(event.currentTarget.value)}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input 
+              type="number" 
+              id="amount" 
+              value={enteredAmount} 
+              onChange={event => setEnteredAmount(event.target.value)}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
